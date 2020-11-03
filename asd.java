cced603810d4076d24e8b1914be328f6fcdd26ca
kiku <kiku@itcast.com>
2020-11-03 15:52:51 +0800

    第一次提交

---
+++ asd.java
@@ -0,0 +1,12 @@
+public class asd{
+public static void main(String[] args){
+System.out.println("   *                   *            ");
+System.out.println(" *     *I love Java *     *          ");
+System.out.println("   *                   *            ");
+System.out.println("     *               *              ");
+System.out.println("       *           *                ");
+System.out.println("         *       *                  ");
+System.out.println("           *   *                    ");
+System.out.println("             *                      ");
+}
+}
