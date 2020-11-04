12d06d50195d71260ac134d5fc9d9eb30beb8212
kiku <kiku@itcast.com>
2020-11-04 21:50:21 +0800

    把本地工程导入git

---
+++ git_test/src/com/itheima/Test.java
@@ -0,0 +1,11 @@
+package com.itheima;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("axcascabciucguac");
+        System.out.println("axcascabciucguac");
+        System.out.println("axcascabciucguac");
+        System.out.println("axcascabciucguac");
+        System.out.println("axcascabciucguac");
+    }
+}
