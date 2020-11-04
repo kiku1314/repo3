3f638b3484c2ca505d60933c8d9ded2305f60047
kiku <kiku@itcast.com>
2020-11-04 22:18:45 +0800

    修改了代码

--- git_test/src/com/itheima/Test.java
+++ git_test/src/com/itheima/Test.java
@@ -7,11 +7,13 @@ public class Test {
         System.out.println("axcascabciucguac");
         System.out.println("axcascabciucguac");
         System.out.println("axcascabciucguac");
+//        hello git
     }
 
     public void test(){
         int i = 0;
         int j = 0;
         i += 10;
+        System.out.println(i);
     }
 }
